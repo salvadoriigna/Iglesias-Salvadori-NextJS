879298eb642a9a022b0619479652af6b3eb19d8e
salvadoriigna <isalvadori@pioix.edu.ar>
2025-08-16 14:01:24 -0300

    Update page.js

--- src/app/contador/page.js
+++ src/app/contador/page.js
@@ -3,21 +3,24 @@
 import Button from "@/components/Button"
 import Input from "@/components/Input"
 import Title from "@/components/Title"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function ContadorPage() {
     const [cuenta, setCuenta] = useState(0)
     const [chequear, setChequear] = useState(false)
 
-    function incrementar() {
-        setCuenta(cuenta + 1)
-        if (cuenta == 20){
-            setCuenta(0)
+    function cambiarCuenta() {
+        let valor = cuenta 
+        if (chequear) {
+            valor = cuenta + 1
+        } else {
+            valor = cuenta - 1
         }
-    }
 
-    function decrementar() {
-        setCuenta(cuenta - 1)
+        if (valor == 20) {
+            valor = 0
+        }
+        setCuenta(valor)
     }
 
     function checkBoxCambio(event) {
@@ -27,10 +30,7 @@ export default function ContadorPage() {
     return (
         <>
             <Title title="Pagina del contador"></Title>
-            <Button onClick={incrementar} text="Incrementar"></Button>
-            <br></br>
-            <Button onClick={decrementar} text="Decretementar"></Button>
-            <br></br>
+            <Button onClick={cambiarCuenta} text="Incrementar/decrementar"></Button>
             <Input type="checkbox" onChange={checkBoxCambio}></Input>
             <h2>{cuenta}</h2>
         </>
